922513b259d956431c880b901aeac1d3d18c4aec
hi-san <h.ahkh10@icloud.com>
2025-04-25 18:50:43 +0900

    UsersTableSeeder作成、開発期間用のダミーデータ追加

---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $content = [
+            'name' => 'mm',
+            'email' => 'm@mm',
+            'password' => Hash::make('00000000'),
+            'email_verified_at' => CarbonImmutable::today()
+        ];
+
+        DB::table('users')->insert($content);
+    }
+}
