29cef00fe89613d8cd00fdde035a4e24a4dc3397
hi-san <h.ahkh10@icloud.com>
2025-05-18 12:40:53 +0900

    AttendanceFactory修正

--- src/database/factories/AttendanceFactory.php
+++ src/database/factories/AttendanceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class AttendanceFactory extends Factory
@@ -13,11 +14,13 @@ class AttendanceFactory extends Factory
      */
     public function definition()
     {
+        $date = new CarbonImmutable('08:00:00');
+
         return [
             'user_id' => $this->faker->numberBetween($min = 1, $max = 10),
-            'date' => $this->faker->dateTimeBetween('-1 week', '+1 week'),
-            'start_time' => $this->faker->time(),
-            'end_time' => $this->faker->time(),
+            'date' => $this->faker->dateTimeBetween('-2month', '+2month'),
+            'start_time' => $this->faker->dateTimeBetween($date, $date->modify('+2hour'))->format('H:i:s'),
+            'end_time' => $this->faker->dateTimeBetween($date->modify('+8hour'), $date->modify('+10hour'))->format('H:i:s'),
             'rest_total_time' => $this->faker->time(),
             'total_working_time' => $this->faker->time(),
         ];
