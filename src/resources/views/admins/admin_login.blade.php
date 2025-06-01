fa35679dd7e57c7700548bce6c9362db65b379dc
hi-san <h.ahkh10@icloud.com>
2025-06-01 09:29:49 +0900

    管理者ログイン画面装飾

--- src/resources/views/admins/admin_login.blade.php
+++ src/resources/views/admins/admin_login.blade.php
@@ -15,20 +15,20 @@
             @csrf
             <div class="form-item">
                 <p class="form-item__text">メールアドレス</p>
-                <input type="email" name="email" value="{{ old('email') }}">
+                <input type="email" class="form-item__input" name="email" value="{{ old('email') }}">
                 @error('email')
                 <p class="error-message">{{ $message }}</p>
                 @enderror
             </div>
             <div class="form-item">
                 <p class="form-item__text">パスワード</p>
-                <input type="password" name="password">
+                <input type="password" class="form-item__input" name="password">
                 @error('password')
                 <p class="error-message">{{ $message }}</p>
                 @enderror
             </div>
             <div class="form-item">
-                <input type="submit" value="管理者ログインする">
+                <input type="submit" class="form-item__submit" value="管理者ログインする">
             </div>
         </form>
     </div>
