39c1f919b9c0523093bfe428fcdcaa9fb91ebfcd
hi-san <h.ahkh10@icloud.com>
2025-06-01 09:34:51 +0900

    勤怠一覧画面(管理者)装飾

--- src/resources/views/admins/attendance_list.blade.php
+++ src/resources/views/admins/attendance_list.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('css')
+<link rel="stylesheet" href="{{ asset('css/list.css') }}">
 @endsection
 
 @section('header')
-    @include('layouts/admin')
+@include('layouts/admin')
 @endsection
 
 @section('content')
@@ -13,19 +14,18 @@
         <h1 class="title-text">{{ $carbon->format('Y年m月d日') }}の勤怠一覧</h1>
     </div>
     <div class="attendance_list-date">
-        <a href="{{ route('admin_attendance_list', ['day' => $previousDay]) }}" class="date">←前日</a>
+        <a href="{{ route('admin_attendance_list', ['day' => $previousDay]) }}" class="date gray">←前日</a>
         <p class="date">{{ $carbon->format('Y/m/d') }}</p>
-        <a href="{{ route('admin_attendance_list', ['day' => $nextDay]) }}" class="date">→翌日</a>
+        <a href="{{ route('admin_attendance_list', ['day' => $nextDay]) }}" class="date gray">→翌日</a>
     </div>
     <table>
-        <tr>
-            <th>名前</th>
+        <tr class="top">
+            <th class="left-top">名前</th>
             <th>出勤</th>
             <th>退勤</th>
             <th>休憩</th>
             <th>合計</th>
-            <th></th>
-            <th>詳細</th>
+            <th class="right-top">詳細</th>
         </tr>
         @foreach($attendances as $attendance)
         <tr>
@@ -46,5 +46,6 @@
         </tr>
         @endforeach
     </table>
+    <div class="bottom"></div>
 </div>
 @endsection
