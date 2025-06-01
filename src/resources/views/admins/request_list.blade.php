5e12e0d6e588e776d717f5f4fb7e36185e79de37
hi-san <h.ahkh10@icloud.com>
2025-06-01 09:48:45 +0900

    申請一覧画面(管理者)装飾

--- src/resources/views/admins/request_list.blade.php
+++ src/resources/views/admins/request_list.blade.php
@@ -1,29 +1,30 @@
 @extends('layouts/app')
 
 @section('css')
+<link rel="stylesheet" href="{{ asset('css/request_list.css') }}">
 @endsection
 
 @section('header')
-    @include('layouts/admin')
+@include('layouts/admin')
 @endsection
 
 @section('content')
 <div class="request_list-container">
     <div class="request_list-title">
-        <h1 class="title__text">申請</h1>
+        <h1 class="title-text">申請一覧</h1>
     </div>
     <div class="status_bar">
-        <a href="{{ route('request_list') }}">承認待ち</a>
-        <a href="{{ route('request_list') }}?page=approved">承認済み</a>
+        <a href="{{ route('request_list') }}" class="status_bar__link" @if(is_null($prm)) style="color: red;" @endif>承認待ち</a>
+        <a href="{{ route('request_list') }}?page=approved" class="status_bar__link" @if($prm=="approved" ) style="color:red;" @endif>承認済み</a>
     </div>
     <table>
         <tr>
-            <th>状態</th>
+            <th class="left-top">状態</th>
             <th>名前</th>
             <th>対象日時</th>
             <th>申請理由</th>
             <th>申請日時</th>
-            <th>詳細</th>
+            <th class="right-top">詳細</th>
         </tr>
         @foreach($correction_requests as $correction_request)
         <tr>
@@ -38,9 +39,10 @@
             <th>{{ $correction_request->target_date->format('Y/m/d') }}</th>
             <th>{{ $correction_request->request_reason }}</th>
             <th>{{ $correction_request->request_date->format('Y/m/d') }}</th>
-            <th><a href="{{ route('approval_detail', ['attendance_correct_request' => $correction_request->id]) }}">詳細</a></th>
+            <th><a href="{{ route('approval_detail', ['attendance_correct_request' => $correction_request->id]) }}" class="detail">詳細</a></th>
         </tr>
         @endforeach
     </table>
+    <div class="bottom"></div>
 </div>
 @endsection
