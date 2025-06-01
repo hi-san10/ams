2c891a49d1137fc0bb963bda9a78fa33ef090d4d
hi-san <h.ahkh10@icloud.com>
2025-06-01 09:43:16 +0900

    スタッフ一覧画面装飾

--- src/resources/views/admins/staff_list.blade.php
+++ src/resources/views/admins/staff_list.blade.php
@@ -1,30 +1,32 @@
 @extends('layouts.app')
 
 @section('css')
+<link rel="stylesheet" href="{{ asset('css/list.css') }}">
 @endsection
 
 @section('header')
-    @include('layouts/admin')
+@include('layouts/admin')
 @endsection
 
 @section('content')
 <div class="staff_list-container">
-    <h1 class="container__title">スタッフ一覧</h1>
-</div>
-<div class="list-inner">
+    <div class="staff_list-title">
+        <h1 class="title-text">スタッフ一覧</h1>
+    </div>
     <table>
         <tr>
-            <th>名前</th>
+            <th class="left-top">名前</th>
             <th>メールアドレス</th>
-            <th>月次勤怠</th>
+            <th class="right-top">月次勤怠</th>
         </tr>
         @foreach($users as $user)
         <tr>
             <td>{{ $user->name }}</td>
             <td>{{ $user->email }}</td>
-            <td><a href="{{ route('staff_attendance_list', ['id' => $user->id]) }}">詳細</a></td>
+            <td><a href="{{ route('staff_attendance_list', ['id' => $user->id]) }}" class="detail">詳細</a></td>
         </tr>
         @endforeach
     </table>
+    <div class="bottom"></div>
 </div>
 @endsection
