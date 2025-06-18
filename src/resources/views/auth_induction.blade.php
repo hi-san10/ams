3cc924a7bf25a84601377b99c9ac7ab3779e690c
hi-san <h.ahkh10@icloud.com>
2025-06-18 17:37:21 +0900

    メール認証動線変更

--- src/resources/views/auth_induction.blade.php
+++ src/resources/views/auth_induction.blade.php
@@ -11,7 +11,7 @@
         <p class="inner__text">メール認証を完了してください。</p>
     </div>
     <div class="verification__link">
-        <a href="{{ route('verification', ['email' => $email]) }}" class="link-content">認証はこちらから</a>
+        <a href="https://mailtrap.io" class="link-content">認証はこちらから</a>
     </div>
     <div class="resend__link">
         <a href="{{ route('resend', ['email' => $email]) }}" class="link-content--blue">認証メールを再送する</a>
