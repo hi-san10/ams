6822a0ec55e828d12e41bc53f2445c63d28f9702
hi-san <h.ahkh10@icloud.com>
2025-06-18 17:44:35 +0900

    top画面装飾

--- src/resources/views/select.blade.php
+++ src/resources/views/select.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts/app')
 
 @section('css')
+<link rel="stylesheet" href="{{ asset('css/select.css') }}">
 @endsection
 
 @section('content')
 <div class="select-container">
-    <a href="/admin/login">管理者はこちら</a>
-    <a href="/login">一般ユーザーはこちら</a>
+    <a class="link" href="/admin/login">管理者はこちら</a>
+    <a class="link" href="/login">一般ユーザーはこちら</a>
 </div>
 @endsection
