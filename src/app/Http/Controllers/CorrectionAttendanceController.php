596acec1e5e7c544fddba749de49afc51e62eeef
hi-san <h.ahkh10@icloud.com>
2026-03-07 14:22:11 +0900

    refactor: 修正申請処理をserviceに分離

--- src/app/Http/Controllers/CorrectionAttendanceController.php
+++ src/app/Http/Controllers/CorrectionAttendanceController.php
@@ -4,48 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Attendance;
-use App\Models\CorrectionAttendance;
-use App\Models\CorrectionRest;
 use App\Models\StampCorrectionRequest;
-use Carbon\CarbonImmutable;
 use App\Http\Requests\CorrectionRequest;
+use App\Services\CorrectionService;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class CorrectionAttendanceController extends Controller
 {
+    public function __construct(CorrectionService $correctionService)
+    {
+        $this->correctionService = $correctionService;
+    }
+
     public function correction(CorrectionRequest $request, Attendance $attendance)
     {
-        DB::transaction(function () use(
-            $request,
-            $attendance,
-        ) {
-            $correction = StampCorrectionRequest::create([
-                'user_id' => Auth::id(),
-                'attendance_id' => $attendance->id,
-                'target_date' => $attendance->date,
-                'request_date' => CarbonImmutable::today(),
-                'request_reason' => $request->remarks
-                ]);
-
-            $correction_attendance = CorrectionAttendance::create([
-                'stamp_correction_request_id' => $correction->id,
-                'start_time' => $request->start,
-                'end_time' => $request->end,
-            ]);
-
-            $rests = [];
-            foreach ($request->rests as $rest) {
-                if (blank($rest['start_time']) || blank($rest['end_time'])) {
-                    continue;
-                }
-                $rests[] = [
-                    'start_time' => $rest['start_time'],
-                    'end_time' => $rest['end_time'],
-                ];
-            }
-            $correction_attendance->rests()->createMany($rests);
-        });
+        $this->correctionService->correction($request, $attendance);
 
         return redirect()->route('attendance_list');
     }
